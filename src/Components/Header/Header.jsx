7bce386ff6b7e378e6360153483f2ac861171963
Sonal Sai K S <64025222+sonalsai@users.noreply.github.com>
2025-07-10 23:03:02 +0530

    Enhance Header component: add 'Add Note' button and style it

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,7 @@ import "./Header.scss";
 const Header = () => {
   return (
     <div className="headerContainer">
+      {/* Title */}
       <div className="titleContainer">
         <img
           src="https://cdn-icons-png.flaticon.com/512/1055/1055642.png"
@@ -14,6 +15,11 @@ const Header = () => {
           <h1 className="headerTitle">EasNote</h1>
           <span className="headerSubtitle">Your Easy Note-Taking App</span>
         </div>
+      </div>
+
+      {/* Add Note Button */}
+      <div className="addNoteButtonContainer">
+        <button className="addNoteButton">Add Note</button>
       </div>
     </div>
   );
