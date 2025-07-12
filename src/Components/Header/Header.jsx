2c7a0f80a90375f3ca98d2e43a7312792a8177e2
Sonal Sai K S <64025222+sonalsai@users.noreply.github.com>
2025-07-12 19:02:33 +0530

    Add note options to Header component with corresponding icons and styles for improved navigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import "./Header.scss";
 // import Logo from "../../assets/logo.png";
-import PlusIcon from "../../assets/plusIcon.svg";
+import addNotesIcon from "../../assets/addNotesIcon.svg";
+import allNotesIcon from "../../assets/allNotesIcon.svg";
+import favoriteNoteIcon from "../../assets/favoriteNoteIcon.svg";
+import lockedNoteIcon from "../../assets/lockedNoteIcon.svg";
+import deleteIcon from "../../assets/deleteIcon.svg";
 
-const Header = ({setShowAddNoteForm}) => {
+const Header = ({ setShowAddNoteForm }) => {
   return (
     <div className="headerContainer">
       {/* Title */}
@@ -18,9 +22,35 @@ const Header = ({setShowAddNoteForm}) => {
         </div>
       </div>
 
+      {/* Note Options */}
+      <div className="noteOptionsContainer">
+        <button className="noteOptionButton">
+          <img src={allNotesIcon} alt="" />
+          All Notes
+        </button>
+        <button className="noteOptionButton">
+          <img src={favoriteNoteIcon} alt="" />
+          Favorites Notes
+        </button>
+        <button className="noteOptionButton">
+          <img src={lockedNoteIcon} alt="" />
+          Locked Notes
+        </button>
+        <button className="noteOptionButton">
+          <img src={deleteIcon} alt="" />
+          Recycle Bin</button>
+      </div>
+
       {/* Add Note Button */}
       <div className="addNoteButtonContainer">
-        <button className="addNoteButton" onClick={()=> setShowAddNoteForm(true)}> <img src={PlusIcon} alt="" className="addNoteIcon" />Add Note</button>
+        <button
+          className="addNoteButton"
+          onClick={() => setShowAddNoteForm(true)}
+        >
+          {" "}
+          <img src={addNotesIcon} alt="" className="addNoteIcon" />
+          Add Note
+        </button>
       </div>
     </div>
   );
